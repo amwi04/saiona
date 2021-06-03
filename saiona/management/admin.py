1db51d2f0c7de62537f154424c98b2adbc696560
Amod Wani <amodwani04@gmail.com>
2021-06-04 01:10:34 +0530

    bug fixed

--- saiona/management/admin.py
+++ saiona/management/admin.py
@@ -7,19 +7,19 @@ class Client_type_admin(admin.ModelAdmin):
     list_display = ('pk','name')
 
 class Clients_admin(admin.ModelAdmin):
-    list_display = ('pk','name','gst_no','address','type_name')
+    list_display = ('pk','name','gst_no','address','type_name','balance')
 
 class Product_admin(admin.ModelAdmin):
     list_display =  ('pk','product_name','product_quantity',
-                'product_cost','product_transport_cost',
-                'product_pack_cost','product_total_cost',
+                'product_cost','product_transport_cost','total_product_batch',
+                'product_pack_cost','product_total_cost', 'product_on_batch_sold',
                 'product_selling_cost','product_batch_no','product_status')
 
 class AddOrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('order_quantity',
-                        'order_product','client',
+                        'product','client',
                         'purchase_date')
 
                 
@@ -28,10 +28,15 @@ class Order_admin(admin.ModelAdmin):
     exclude = ('orders_product_cost','orders_product_selling')
 
     list_display = ('pk','order_quantity',
-                        'order_product','order_product_cost',
+                        'product','order_product_cost','order_taxed_amount',
                          'order_product_selling', 'client','purchase_date')
+
+
+class Payments_admin(admin.ModelAdmin):
+    list_display = ('client','amount_paid')
 
 admin.site.register(Client_type,Client_type_admin)
 admin.site.register(Clients,Clients_admin)
 admin.site.register(Product,Product_admin)
 admin.site.register(Order,Order_admin)
+admin.site.register(Payments,Payments_admin)
--- saiona/management/models.py
+++ saiona/management/models.py
@@ -24,7 +24,8 @@ class Clients(models.Model):
     type_name = models.ForeignKey(Client_type,on_delete=models.DO_NOTHING,
                                     max_length=20, blank=True, null=True,
                                      related_name='client_type')
-    
+    balance = models.FloatField(_('balance'),help_text='Balance need to be paid',default=0)
+
     def __str__(self) -> str:
         return self.name
     
@@ -45,14 +46,15 @@ class Product(models.Model):
 
     product_total_cost = models.FloatField(_('total_cost'),help_text='Product actual cost+transport+packing',blank=True,null=True)
 
+    total_product_batch = models.IntegerField(_('total_product_batch'),help_text='Total product made on this batch no',blank=True,null=True)
+    product_on_batch_sold = models.IntegerField(_('product_sold'),default=0,blank=True,null=True,help_text='When sold addin diff will be product left')
     product_selling_cost = models.FloatField(_('selling_cost'),help_text='Selling Price per piece')
     product_batch_no = models.CharField(_('Batch No'),max_length=20)
     product_status = models.BooleanField(_('Status'),help_text='If product with batch is still in sell 1 YES 0 NO')
 
     def save(self,*args, **kwargs):
-        if not self.product_total_cost:
-            self.product_total_cost = self.product_cost + self.product_transport_cost + self.product_pack_cost
-            super(Product, self).save(*args, **kwargs)
+        self.product_total_cost = self.product_cost + self.product_transport_cost + self.product_pack_cost
+        super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.product_name
@@ -69,8 +71,8 @@ class Order(models.Model):
         verbose_name_plural = _("Orders")
 
 
-    order_quantity = models.IntegerField(_('quantity'),help_text='Order Quantity per product')
-    order_product = models.ForeignKey(Product,
+    order_quantity = models.IntegerField(_('quantity'),default=0,help_text='Order Quantity per product')
+    product = models.ForeignKey(Product,name='product',
                                     on_delete=models.DO_NOTHING,
                                     limit_choices_to={'product_status':True})
     order_product_cost = models.FloatField(_('product_cost'),help_text='Product cost on total order quantity',
@@ -79,18 +81,33 @@ class Order(models.Model):
                                                 blank=True,null=True)
     order_by_client = models.ForeignKey(Clients,name='client', on_delete=models.DO_NOTHING)
     date_of_purchase = models.DateTimeField(name='purchase_date',default=timezone.now)
-    tax = models.FloatField('Tax percentage')
+    tax = models.FloatField('Tax percentage',default=12)
     order_taxed_amount = models.FloatField(_('taxed_amount'),blank=True,null=True)
 
     def save(self, *args, **kwargs):
         if not self.id:
-            self.order_product_cost = self.order_product.product_total_cost * self.order_quantity
-            self.order_product_selling = self.order_product.product_selling_cost * self.order_quantity
+            self.order_product_cost = self.product.product_total_cost * self.order_quantity
+            self.order_product_selling = self.product.product_selling_cost * self.order_quantity
             self.order_taxed_amount = self.order_product_selling + (self.tax * self.order_product_selling / 100)
+            self.client.balance += self.order_taxed_amount 
+            self.product.product_on_batch_sold += self.order_quantity 
+            self.product.save()
+            self.client.save()
             super(Order, self).save(*args, **kwargs)
 
     def __str__(self):
-        return self.order_product.product_name
+        return self.product.product_name
 
     def get_absolute_url(self):
         return reverse("Order_detail", kwargs={"pk": self.pk})
+
+
+class Payments(models.Model):
+    client = models.ForeignKey(Clients,on_delete=models.DO_NOTHING)
+    amount_paid = models.FloatField(_('amount_paid'))
+
+    def save(self,*args, **kwargs):
+        self.client.balance -= self.amount_paid
+        self.client.save()
+        super(Payments, self).save(*args, **kwargs)
+
